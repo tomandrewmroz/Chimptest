2c05fb9a4f918602f6d220db449596aa328003de
Tomasz Mróz <73473345+tomandrewmroz@users.noreply.github.com>
2024-02-29 10:51:16 +0100

    Highlight collection and upgraded gameplay

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <windows.h>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 int pressedButton = 0;
+HWND buttonHandle;
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -18,6 +20,11 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPar
     
     case WM_COMMAND:
     	pressedButton = wParam;
+    	buttonHandle = (HWND)lParam;
+    	break;
+    	
+    case WM_LBUTTONDOWN:
+    	pressedButton = 1;
     	break;
     
     default:
@@ -27,23 +34,43 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPar
     return 0;
 }
 
-const int lineX = 25, lineY = 25, buttonX = 100, buttonY = 100;
+const int lineX = 25, lineY = 25, buttonX = 100, buttonY = 100, MAX_LEVEL = 36;
 
 struct Menu{
 	const char* title = "Chimptest";
-	int level, *space, spaces, seed, restOfButtons;
-	HWND *buttons, *handle;
+	int level, **space, spaces, seed, restOfButtons;
+	HWND *buttons, *handle, *texts;
 	HINSTANCE *hInstance;
-	Menu(HWND &handle, HINSTANCE &hInstance){
-		this->handle = &handle;
-		this->hInstance = &hInstance;
+	bool waitingToReset;
+	
+	void reset(){
+		for(int i = 0; i < spaces; i++){
+			drawBorder("white", space[i][0]);
+			DestroyWindow(buttons[i]);
+			DestroyWindow(texts[i]);
+		}		
 		level = 0;	
 		restOfButtons = 0;
-		buttons = new HWND[36];	
-		space = new int[36];
 		spaces = 0;
 		seed = time(NULL);
 		srand(seed);
+		waitingToReset = false;
+		delete buttons;
+		delete texts;
+		delete space;
+		buttons = new HWND[MAX_LEVEL];	
+		texts = new HWND[MAX_LEVEL];
+		space = new int* [MAX_LEVEL];
+		for(int i = 0; i < MAX_LEVEL; i++) space[i] = new int[2];
+	}
+	
+	Menu(HWND &handle, HINSTANCE &hInstance){
+		this->handle = &handle;
+		this->hInstance = &hInstance;
+		buttons = new HWND[MAX_LEVEL];	
+		texts = new HWND[MAX_LEVEL];
+		space = new int* [MAX_LEVEL];
+		for(int i = 0; i < MAX_LEVEL; i++) space[i] = new int[2];
 	}
 	
 	bool addButton(){
@@ -53,51 +80,132 @@ struct Menu{
 		
 		int rest = rand() % (36 - spaces) + 1, current = -1;
 		for(int i = 0; i < spaces; i++){
-			int difference = space[i] - current - 1;
+			int difference = space[i][0] - current - 1;
 			if(rest <= difference){
-				for(int j = spaces; j > i; j--)
-					space[j] = space[j - 1];
+				for(int j = spaces; j > i; j--){
+					space[j][0] = space[j - 1][0];
+					space[j][1] = space[j - 1][1];
+				}
 				current += rest;
-				space[i] = current;
+				space[i][0] = current;
+				space[i][1] = spaces; 
 				rest = 0;
 				spaces++;
 				break;
 			}
 			rest -= difference;
-			current = space[i];
+			current = space[i][0];
 		}
 		if(rest){
 			current += rest;
-			space[spaces++] = current;	
+			space[spaces][0] = current;	
+			space[spaces][1] = spaces;
+			spaces++;
 		}
+		texts[spaces - 1] = CreateWindowEx(0, "STATIC", NULL, WS_CHILD | WS_VISIBLE | SS_CENTER, calcX(current), calcY(current), buttonX, buttonY, *handle, NULL, *hInstance, NULL);
+		SetWindowText(texts[spaces - 1], std::to_string(spaces).c_str());
 		
-		int row = current / 6, column = current % 6;
-		buttons[spaces - 1] = CreateWindowEx(0, "BUTTON", "", WS_CHILD | WS_VISIBLE, lineX * (column + 1) + buttonX * column, lineY * (row + 1) + buttonY * row, buttonX, buttonY, *handle, (HMENU)(200 + spaces), *hInstance, NULL);
+		buttons[spaces - 1] = CreateWindowEx(0, "BUTTON", "", WS_CHILD | WS_VISIBLE, calcX(current), calcY(current), buttonX, buttonY, *handle, (HMENU)(200 + spaces), *hInstance, NULL);	
 	}
 	
-	void checkButton(){
-		if(!(::pressedButton)) return;
-		
-		int pressedButton = ::pressedButton - 200;
-		::pressedButton = 0;
-		
+	int indexOfButton(int number){
+		for(int i = 0; i < spaces; i++)
+			if(space[i][1] == number - 1) return space[i][0];
+		return -1;
+	}
+	
+	bool checkButton(){
+		int pressedButton = ::pressedButton - 200, position = indexOfButton(pressedButton);
 		if(pressedButton == level - restOfButtons + 1){
+			ShowWindow(buttons[pressedButton - 1], SW_HIDE);
+			if(position != -1) drawBorder("green", position);
 			restOfButtons--;
-			if(restOfButtons == 0) nextLevel();
+			if(restOfButtons == 0){
+				if(level < MAX_LEVEL) nextLevel();
+				else end(true);
+			}
+			return false;
 		}
+		if(position != -1) drawBorder("red", position);
+		return true;
+	}
+	
+	void check(){
+		if(!pressedButton) return;
+		
+		//MessageBox(*handle, std::to_string(pressedButton).c_str(), "pressedButton message number", MB_OK);
+		
+		if(pressedButton > 200){
+			if(checkButton()) end(false);
+		}
+		else if(waitingToReset){
+			//MessageBox(*handle, std::to_string(pressedButton).c_str(), "pressedButton message number for reset", MB_OK);
+			reset();
+			nextLevel();
+		}
+		pressedButton = 0;
 	}
 	
 	void restoreButtons(){
+		for(int i = 0; i < spaces; i++){
+			drawBorder("white", space[i][0]);
+			ShowWindow(buttons[i], SW_SHOW);
+		}
+	}
+	
+	void hideButtons(){
+		for(int i = 0; i < spaces; i++)
+			ShowWindow(buttons[i], SW_HIDE);
+	}
+	
+	void drawLine(std::string color, int fromX, int fromY, int toX, int toY){
+		COLORREF penColor = (color == "red" ? 0x0000FF : color == "green" ? 0x00FF00 : 0xFFFFFF);
+		HPEN pen = CreatePen(PS_SOLID, 5, penColor);
+		HDC hdc = GetDC(*handle);
+		SelectObject(hdc, pen);
+		MoveToEx(hdc, fromX, fromY, NULL);
+		LineTo(hdc, toX, toY);
+		ReleaseDC(*handle, hdc);
+		DeleteObject(pen);
+	}
+	
+	void drawBorder(std::string color, int number){
+		int x = calcX(number), y = calcY(number);
+		drawLine(color, x, y, x + buttonX, y);	
+		drawLine(color, x, y, x, y + buttonY);
+		drawLine(color, x + buttonX, y, x + buttonX, y + buttonY);
+		drawLine(color, x, y + buttonY, x + buttonX, y + buttonY);
+	}
+	
+	void end(bool success){
+		hideButtons();
 		
+		waitingToReset = true;
 	}
 	
 	void nextLevel(){
+		if(level >= MAX_LEVEL) end(true);
 		restoreButtons();
 		level++;
 		restOfButtons = level;
 		addButton();
 	}
 	
+	int calcRow(int number){
+		return number / 6;
+	}
+	
+	int calcColumn(int number){
+		return number % 6;
+	}
+	
+	int calcX(int number){
+		return lineX * (calcColumn(number) + 1) + buttonX * calcColumn(number);
+	}
+	
+	int calcY(int number){
+		return lineY * (calcRow(number) + 1) + buttonX * calcRow(number);
+	}
 };
 
 bool makeWindow(HINSTANCE &hInstance, WNDCLASSEX &window, HWND &handle, Menu &menu)
@@ -141,6 +249,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	if(makeWindow(hInstance, window, handle, menu)){
 		ShowWindow(handle, nCmdShow);
 		
+		menu.reset();
 		menu.nextLevel();
 		
 		while(GetMessage(&message, NULL, 0, 0)){
@@ -149,7 +258,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		    TranslateMessage(&message);
 		    DispatchMessage(&message);
 		    
-		    menu.checkButton();
+		    menu.check();
 		}
 	    return message.wParam;
 	}
